2550691470f74da885253fe4e1dd454b6d599343
jdj <351403792@qq.com>
2015-10-19 14:58:30 +0800

    jdj

---
+++ src/cn/zucc/day01/HelloWorld.java
@@ -0,0 +1,16 @@
+package cn.zucc.day01;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World!!");
+System.out.println("ĞÕÃû");
+System.out.println("ÄêÁä");
+System.out.println("°®ºÃ");
+	}
+
+}
---
+++ src/cn/zucc/day01/shopping.java
@@ -0,0 +1,37 @@
+package cn.zucc.day01;
+
+public class shopping {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("\t\t购物清单");
+System.out.print("商品名称");
+System.out.print("\t\t购物数量");
+System.out.print("\t\t商品单价");
+System.out.print("\t\t金额\n");
+System.out.print("鳄鱼夹克");
+System.out.print("\t\t1");
+System.out.print("\t\t1900");
+System.out.print("\t\t1900\n");
+System.out.print("ipodmp4");
+System.out.print("\t\t2");
+System.out.print("\t\t400");
+System.out.print("\t\t800\n");
+
+System.out.println("\t\t欢迎使用我行我素购物管理系统");
+System.out.println("******************************************************************");
+System.out.println("\t\t1.客户信息");
+System.out.println("");
+System.out.println("\t\t2.购物结算");
+System.out.println("");
+System.out.println("\t\t3.真情回馈");
+System.out.println("");
+System.out.println("\t\t4.注销");
+System.out.println("******************************************************************");
+	}
+	
+	
+}
